2c193a3068b894337ce3e9e7ac2d366365acd06f
ourlifething <ryuji.hayama@gmail.com>
2023-11-09 15:36:26 +0900

    Note(scriot)作成waterが下に落ちる

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    //Waterが下に落ちる仕様
+    // TODO:落ちる速度を曲と判定場所との距離から設定する必要がある
+    float speed;
+    private void Start() {
+        speed = 20;
+    }
+    void Update()
+    {
+        transform.Translate(0, -speed * Time.deltaTime, 0);
+    }
+}
