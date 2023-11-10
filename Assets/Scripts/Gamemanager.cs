ed1f1b2893f167c89e4d5734e1bc75ed7a0972c9
ourlifething <ryuji.hayama@gmail.com>
2023-11-10 17:15:55 +0900

    timelineにsignal追加、OnEndEventイベントの作成

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -8,6 +8,8 @@ public class Gamemanager : MonoBehaviour
     // TODO : ゲーム開始の実装
     // コルーチンを使う：時間を制御したい 3.2.1のカウントダウン
     //タイムラインのplay on awakeを外してスクリプトでタイムライン再生したい
+    // ゲームの実装：タイムラインの終了を検知する。
+    // シグナル(signal)を使う
     [SerializeField] PlayableDirector playableDirector;
     void Start()
     {
@@ -25,6 +27,11 @@ public class Gamemanager : MonoBehaviour
         yield return new WaitForSeconds(0.3f);
         Debug.Log("ゲーム開始！");
         playableDirector.Play();
+    }
+
+    public void OnEndEvent()
+    {
+        Debug.Log("ゲーム終了");
     }
 
 }
