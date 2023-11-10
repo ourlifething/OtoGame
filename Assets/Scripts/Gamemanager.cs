10162e6d05afc1a76028086044bbf6596d21ccc3
ourlifething <ryuji.hayama@gmail.com>
2023-11-10 16:59:20 +0900

    GamemanagerのplayableDirector作成

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Gamemanager : MonoBehaviour
+{
+    // TODO : ゲーム開始の実装
+    // コルーチンを使う：時間を制御したい 3.2.1のカウントダウン
+    //タイムラインのplay on awakeを外してスクリプトでタイムライン再生したい
+    [SerializeField] PlayableDirector playableDirector;
+    void Start()
+    {
+        StartCoroutine(GameMain());
+    }
+    IEnumerator GameMain()
+    {
+        Debug.Log("3");
+        yield return new WaitForSeconds(1);
+        Debug.Log("2");
+        yield return new WaitForSeconds(1);
+        Debug.Log("1");
+        yield return new WaitForSeconds(1);
+        Debug.Log("Go");
+        yield return new WaitForSeconds(0.3f);
+        Debug.Log("ゲーム開始！");
+        playableDirector.Play();
+    }
+
+}
--- Assets/Scripts/NoteGenerator.cs
+++ Assets/Scripts/NoteGenerator.cs
@@ -10,7 +10,7 @@ public class NoteGenerator : MonoBehaviour
 
     private void Start()
     {
-        SpawnNote();
+        //SpawnNote();
     }
     public void SpawnNote()
     {
